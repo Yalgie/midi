b9b5a863f6c5f96764e9930ec8caacdbedc31525
Daniel Yalg <daniel@thecodingforge.com.au>
2019-11-22 00:09:13 +1000

    starting cra app and scaffolding some stuff

---
+++ src/Keyboard.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import WebMidi from 'webmidi';
+
+
+const keyList = [
+  ["C"],
+  ["C#", "D_"],
+  ["D"],
+  ["D#", "E_"],
+  ["E"],
+  ["F"],
+  ["F#", "G_"],
+  ["G"],
+  ["G#", "A_"],
+  ["A"],
+  ["A#", "B_"],
+  ["B"],
+];
+
+const keys = ["A0", "A#0", "B0"];
+
+var key = 0;
+var octave = 1;
+for (var i = 0; i < 85; i++) {
+    keys.push(keyList[key][0] + "" + octave)
+    key++;
+    if (key === 12) {
+        key = 0;
+        octave++;
+    }
+}
+
+console.log(keys)
+
+export class Keyboard extends Component {
+  state = {
+    input: null,
+    output: null,
+  }
+
+  keyDown = (e) => {
+    console.log(`${e.note.name}${e.note.octave}`);
+  }
+  componentDidMount() {
+    WebMidi.enable(() => {
+      this.setState({
+        input: WebMidi.inputs[0],
+        output: WebMidi.outputs[0],
+      });
+      this.state.input.addListener('noteon', "all", this.keyDown);
+    });
+  }
+  render() {
+    return keys.map(key => {
+      return <div key={key} id={key}></div>
+    })
+  }
+}
--- src/index.js
+++ src/index.js
@@ -4,6 +4,18 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+    
+    // output.playNote("C3", 1, { duration: 2000, velocity: 0.5 });
+
+    // setTimeout(() => {
+    //     output.playNote("D3", 1, { duration: 2000, velocity: 0.5 });
+    // }, 2000);
+
+    // setTimeout(() => {
+    //     output.playNote("E3", 1, { duration: 2000, velocity: 0.5 });
+    // }, 4000);
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
